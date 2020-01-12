154f2f47bb99605d710ee61b3db0234dd8f79f0b
Martinlinux1 <matko.micuch@gmail.com>
2020-01-12 15:30:10 +0100

    Added WiFi location.

--- starfinder.py
+++ starfinder.py
@@ -2,6 +2,7 @@ from astropy.time import Time
 from datetime import datetime
 import math
 from object import Object
+import location
 
 
 def eq_to_alt_az(ra, dec, lat, long):
@@ -32,7 +33,7 @@ def eq_to_alt_az(ra, dec, lat, long):
 
 messier_path = 'data/messier-objects.csv'
 
-print('========ROBOMAZE STARFINDER========')
+print('========aMaze STARFINDER========')
 
 objects = {}
 
@@ -47,30 +48,67 @@ with open(messier_path, 'r') as f:
         sky_object = Object(data[0], data[2], data[4], data[6], data[8], data[9])
 
         objects[sky_object.messier] = sky_object
-        print(sky_object.messier)
+
+location_successful = False
+
+lat = 0
+long = 0
+choice_location = ''
+lat_string = ''
+long_string = ''
+
+while not location_successful:
+    print('Get location from WiFi(poor accuracy)[1] or enter it manually[2]: ')
+    choice_location = input()
+
+    if choice_location == '1':
+        (lat, long) = location.Location.get_location()
+    elif choice_location == '2':
+        print('Enter your latitude[dg min sec]: ')
+        lat_string = input()
+        print('Enter your longitude[dg min sec]: ')
+        long_string = input()
+    else:
+        print('invalid choice')
+        continue
+
+    location_successful = True
 
 while True:
-    print('Enter your latitude[dg min sec]: ')
-    lat_string = input()
-    print('Enter your longitude[dg min sec]: ')
-    long_string = input()
-    print('Enter Messier index of your object: ')
-    messier = input()
+    print('Find object[1] or list messier objects[2]: ')
+    choice = input()
 
-    print('Object info: \nname:', objects[messier].name, '\nConstellation:', objects[messier].constellation,
-          '\nType:', objects[messier].type)
+    if choice == '1':
+        print('Enter Messier index of your object: ')
+        messier = input()
 
-    ra_string = objects[messier].ra
-    dec_string = objects[messier].dec
-    lat_string = lat_string.split(' ')
-    long_string = long_string.split(' ')
-    ra_string = ra_string.split(' ')
-    dec_string = dec_string.split(' ')
-    
-    lat = float(lat_string[0]) + float(lat_string[1]) / 60 + float(lat_string[2]) / 3600
-    long = float(long_string[0]) + float(long_string[1]) / 60 + float(long_string[2]) / 3600
-    ra = float(ra_string[0]) + float(ra_string[1]) / 60 + float(ra_string[2]) / 3600
-    dec = float(dec_string[0]) + float(dec_string[1]) / 60 + float(dec_string[2]) / 3600
+        print('Object info: \nname:', objects[messier].name, '\nConstellation:', objects[messier].constellation,
+              '\nType:', objects[messier].type)
 
-    alt, az = eq_to_alt_az(ra, dec, lat, long)
-    print('Position of the object[az, alt]: ', az, ', ', alt)
+        ra_string = objects[messier].ra
+        dec_string = objects[messier].dec
+        lat_string = lat_string.split(' ')
+        long_string = long_string.split(' ')
+        ra_string = ra_string.split(' ')
+        dec_string = dec_string.split(' ')
+
+        if choice_location == '1':
+            lat = float(lat)
+            long = float(long)
+        else:
+            lat = float(lat_string[0]) + float(lat_string[1]) / 60 + float(lat_string[2]) / 3600
+            long = float(long_string[0]) + float(long_string[1]) / 60 + float(long_string[2]) / 3600
+
+        ra = float(ra_string[0]) + float(ra_string[1]) / 60 + float(ra_string[2]) / 3600
+        dec = float(dec_string[0]) + float(dec_string[1]) / 60 + float(dec_string[2]) / 3600
+
+        alt, az = eq_to_alt_az(ra, dec, lat, long)
+        print('Position of the object[az, alt]: ', az, ', ', alt)
+    elif choice == '2':
+        print(objects)
+        for key, sky_object in objects.items():
+            print('name:', sky_object.name, '\tConstellation:', sky_object.constellation,
+                  '\tType:', sky_object.type, '\tMessier index: ', sky_object.messier)
+
+    else:
+        print('Invalid choice...')
